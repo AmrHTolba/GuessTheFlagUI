c2c18715659cc51e7784200f95b87b3d0a95d311
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-01-20 18:08:57 +0200

    Setup Basic UI
    Created the basic UI structure with files

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,15 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Variables
+    @State private var showAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    // MARK: - Body
     var body: some View {
-        VStack(alignment: .leading){
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:30){
+            Text("Tap the Flag of:")
+            Text(countries[correctAnswer])
+        }
+        ForEach(0..<3) { number in
+            Button {
+                
+            } label: {
+                Image(countries[number])
+            }
             
         }
-        .padding()
     }
 }
 
